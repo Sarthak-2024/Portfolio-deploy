77ff97d414713ef23934f7145ed134724bc7009c
Sarthak-2024 <96854872+Sarthak-2024@users.noreply.github.com>
2022-08-09 21:17:26 +0530

    latest commit

---
+++ build/precache-manifest.4f816f265241e838ead804e884dad2be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e0600c60c0703d53ee7c5cd0b21869",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a79d00a624bc54ec78de",
+    "url": "/static/css/main.22337fdc.chunk.css"
+  },
+  {
+    "revision": "48d9187786e26c098e15",
+    "url": "/static/js/2.c13e7ac0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.c13e7ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79d00a624bc54ec78de",
+    "url": "/static/js/main.2a74d011.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/static/media/nodejs.94cafb0d.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/static/media/react.9a28da9f.svg"
+  }
+]);
